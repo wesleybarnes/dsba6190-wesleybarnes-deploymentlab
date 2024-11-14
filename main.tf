c609f3ff3ae788a68136b7675cb3154b6e6cc604
Wesley Barnes <79825400+wesleybarnes@users.noreply.github.com>
2024-11-13 19:50:59 -0500

    Lab Updates
    Updates for the DSBA 6190 Lab

--- main.tf
+++ main.tf
@@ -41,4 +41,59 @@ resource "azurerm_storage_account" "storage" {
   account_replication_type = "LRS"
 
   tags = local.tags
+  is_hns_enabled           = true
+}
+
+// Virtual Network
+
+resource "azurerm_virtual_network" "vn" {
+  name                = "vnet-${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+// Subnet
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "vnet-${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.rg.name
+  address_prefixes     = ["10.0.2.0/24"]
+  service_endpoints    = ["Microsoft.Sql"]
+}
+
+// SQL Server
+
+resource "azurerm_mssql_server" "sqlserver" {
+  name                         = "sql-${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}"
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+  administrator_login          = "4dm1n157r470r"
+  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+}
+
+// SQL Database
+
+resource "azurerm_mssql_database" "database" {
+  name         = "db-${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}"
+  server_id    = azurerm_mssql_server.rg.id
+  collation    = "SQL_Latin1_General_CP1_CI_AS"
+  license_type = "LicenseIncluded"
+  max_size_gb  = 2
+  sku_name     = "Basic"
+  enclave_type = "VBS"
+
+  tags = {
+    foo = "bar"
+  }
+}
+
+// Virtual Network Rule
+
+resource "azurerm_mssql_virtual_network_rule" "network-rule" {
+  name      = "sql-vnet-rule"
+  server_id = azurerm_mssql_server.rg.id
+  subnet_id = azurerm_subnet.rg.id
 }
